fc655a9ad377236dd2de1582f837dda7abb45b24
Ando <=2026034@fsc-educ.com>
2026-06-12 15:15:29 +0900

    完成

--- src/main/java/com/example/blog_app/BlogController.java
+++ src/main/java/com/example/blog_app/BlogController.java
@@ -1,12 +1,32 @@
 package com.example.blog_app;
-
+import java.util.Optional;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class BlogController {
+
+    private final BlogService blogService;
+
+    public BlogController(BlogService blogService) {
+        this.blogService = blogService;
+    }
+
     @GetMapping("/blogs")
-    public String blogs(){
+    public String blogs(Model model){
+        model.addAttribute("blogs", blogService.findAll());
         return "blogs";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogabout(@PathVariable long id, Model model){
+        Optional<Blog> blogOpt = blogService.findById(id);
+        if(blogOpt.isEmpty()){
+            return "redirect:/blogs";
+        }
+        model.addAttribute("blog", blogOpt.get());
+        return "blog/detail";
     }
 }
---
+++ src/main/java/com/example/blog_app/BlogRepository.java
@@ -0,0 +1,33 @@
+package com.example.blog_app;
+
+import java.util.Optional;
+import java.util.List;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BlogRepository {
+
+    private final JdbcClient jdbcClient;
+
+    public BlogRepository(JdbcClient jdbcClient){
+        this.jdbcClient = jdbcClient;
+    }
+
+    public List<Blog> findAll() {
+        return jdbcClient.sql("SELECT id, title, notes FROM blogs")
+            .query(Blog.class)
+            .list();
+   
+    }
+
+    public Optional<Blog> findById(Long id) {
+        return jdbcClient.sql("SELECT id, title, notes FROM blogs WHERE id = :id")
+            .param("id", id)
+            .query(Blog.class)
+            .optional();
+   
+    }
+
+
+}
